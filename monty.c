1f49af31422fb4197fac0e8e9f377dc9ec2514fb
Michael Adebayo <mikerockmusic51@gmail.com>
2022-12-14 16:16:26 +0000

    tabs

--- monty.c
+++ monty.c
@@ -2,22 +2,20 @@
 
 int main(int ac, char *av[])
 {
-    char *error;
-    int fd, fstat, error_size;
-    FILE *file;
-    struct stat st;
+        char *error;
+        int fd, fstat, error_size;
+        FILE *file;
 
-    if (ac != 2)
-    {
-        fprintf(stderr, "USAGE: monty file\n");
-        exit(EXIT_FAILURE);
-    }
+        if (ac != 2)
+        {
+            fprintf(stderr, "USAGE: monty file\n");
+            exit(EXIT_FAILURE);
+        }
 
-    file = fopen(av[1], "r");
-    if (file == NULL)
-    {
-        fprintf(stderr, "Error: Can't open file %s\n", av[1]);
-        exit(EXIT_FAILURE);
-    }
-    
+        file = fopen(av[1], "r");
+        if (file == NULL)
+        {
+            fprintf(stderr, "Error: Can't open file %s\n", av[1]);
+            exit(EXIT_FAILURE);
+        }
 }
